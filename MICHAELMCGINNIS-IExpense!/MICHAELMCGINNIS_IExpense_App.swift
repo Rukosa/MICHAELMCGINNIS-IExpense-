ea5994d534f29351a44d6167e75c84f2f6b92d56
Rukosa <mwmcginnis490@gmail.com>
2022-03-09 12:47:24 -0800

    Initial Commit

---
+++ MICHAELMCGINNIS-IExpense!/MICHAELMCGINNIS_IExpense_App.swift
@@ -0,0 +1,17 @@
+//
+//  MICHAELMCGINNIS_IExpense_App.swift
+//  MICHAELMCGINNIS-IExpense!
+//
+//  Created by Michael Mcginnis on 3/9/22.
+//
+
+import SwiftUI
+
+@main
+struct MICHAELMCGINNIS_IExpense_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
